8ad8a2d93ac531a9dbf5a449e9a9dfd68111fa8d
Timo Stülten <8837182+stuelten@users.noreply.github.com>
2026-01-26 13:06:47 +0100

    chore: Refactor `FileServVersionFilter` for clarity and add `slf4j-simple` dependency in SMB password module

--- fileserv-core/src/main/java/de/sty/fileserv/core/FileServVersionFilter.java
+++ fileserv-core/src/main/java/de/sty/fileserv/core/FileServVersionFilter.java
@@ -8,17 +8,18 @@ import java.io.IOException;
 import static de.sty.fileserv.core.WebDavConstants.HEADER_SERVER;
 
 /**
- * A filter that adds a "Server" header to every response using values from a properties file.
+ * A filter that adds a "Server" header to every response using
+ * {@link FileServVersion#NAME} and {@link FileServVersion#VERSION}.
  */
 public final class FileServVersionFilter implements Filter {
 
-    private static final String SERVER_HEADER = FileServVersion.NAME + "/" + FileServVersion.VERSION;
+    private static final String HEADER_SERVER_VALUE = FileServVersion.NAME + "/" + FileServVersion.VERSION;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         if (response instanceof HttpServletResponse httpResponse) {
-            httpResponse.setHeader(HEADER_SERVER, SERVER_HEADER);
+            httpResponse.setHeader(HEADER_SERVER, HEADER_SERVER_VALUE);
         }
         chain.doFilter(request, response);
     }
